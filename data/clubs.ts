87e19b2e65f900f9232e09092b3af8a39df4a8d9
yersyandev <ysdev22@gmail.com>
2024-08-12 22:10:21 +0400

    added data

--- data/clubs.ts
+++ data/clubs.ts
@@ -180,7 +180,211 @@ const clubs: Team[] = [
         "name": "Eintracht Frankfurt",
         "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/GER",
         "entity": 4
-    }
+    },
+    {
+        "id": 31,
+        "name": "Lazio",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/ITA",
+        "entity": 2
+    },
+    {
+        "id": 32,
+        "name": "PSV",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/NED",
+        "entity": 6
+    },
+    {
+        "id": 33,
+        "name": "Marseille",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/FRA",
+        "entity": 5
+    },
+    {
+        "id": 34,
+        "name": "Slavia Praha",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/CZE",
+        "entity": 10
+    },
+    {
+        "id": 35,
+        "name": "Sporting CP",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/POR",
+        "entity": 7
+    },
+    {
+        "id": 36,
+        "name": "Fiorentina",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/ITA",
+        "entity": 2
+    },
+    {
+        "id": 37,
+        "name": "Lille",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/FRA",
+        "entity": 5
+    },
+    {
+        "id": 38,
+        "name": "Dinamo Zagreb",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/CRO",
+        "entity": 19
+    },
+    {
+        "id": 39,
+        "name": "AZ Alkmaar",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/NED",
+        "entity": 6
+    },
+    {
+        "id": 40,
+        "name": "Shakhtar Donetsk",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/UKR",
+        "entity": 21
+    },
+    {
+        "id": 41,
+        "name": "Salzburg",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/AUT",
+        "entity": 15
+    },
+    {
+        "id": 42,
+        "name": "Rennes",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/FRA",
+        "entity": 5
+    },
+    {
+        "id": 43,
+        "name": "Braga",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/POR",
+        "entity": 7
+    },
+    {
+        "id": 44,
+        "name": "Olympiacos",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/GRE",
+        "entity": 13
+    },
+    {
+        "id": 45,
+        "name": "Tottenham",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/ENG",
+        "entity": 1
+    },
+    {
+        "id": 46,
+        "name": "Copenhagen",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/DEN",
+        "entity": 14
+    },
+    {
+        "id": 47,
+        "name": "Fenerbahçe",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/TUR",
+        "entity": 9
+    },
+    {
+        "id": 48,
+        "name": "Gent",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/BEL",
+        "entity": 8
+    },
+    {
+        "id": 49,
+        "name": "PAOK",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/GRE",
+        "entity": 13
+    },
+    {
+        "id": 50,
+        "name": "Crvena zvezda",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/SRB",
+        "entity": 20
+    },
+    {
+        "id": 51,
+        "name": "Maccabi Tel Aviv",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/ISR",
+        "entity": 12
+    },
+    {
+        "id": 52,
+        "name": "Real Betis",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/ESP",
+        "entity": 3
+    },
+    {
+        "id": 53,
+        "name": "Basel",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/SUI",
+        "entity": 16
+    },
+    {
+        "id": 54,
+        "name": "Qarabağ",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/AZE",
+        "entity": 23
+    },
+    {
+        "id": 55,
+        "name": "Ferencváros",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/HUN",
+        "entity": 22
+    },
+    {
+        "id": 56,
+        "name": "Young Boys",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/SUI",
+        "entity": 16
+    },
+    {
+        "id": 57,
+        "name": "Bodø/Glimt",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/NOR",
+        "entity": 11
+    },
+    {
+        "id": 58,
+        "name": "Freiburg",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/GER",
+        "entity": 4
+    },
+    {
+        "id": 59,
+        "name": "Slovan Bratislava",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/SVK",
+        "entity": 25
+    },
+    {
+        "id": 60,
+        "name": "Union SG",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/BEL",
+        "entity": 8
+    },
+    {
+        "id": 61,
+        "name": "Viktoria Plzeň",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/CZE",
+        "entity": 10
+    },
+    {
+        "id": 62,
+        "name": "Molde",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/NOR",
+        "entity": 11
+    },
+    {
+        "id": 63,
+        "name": "Galatasaray",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/TUR",
+        "entity": 9
+    },
+    {
+        "id": 64,
+        "name": "LASK",
+        "image": "https://api.fifa.com/api/v3/picture/flags-sq-2/AUT",
+        "entity": 15
+    },
 ];
 
 export default clubs;
